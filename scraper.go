ef3d18d2f74e92353c724e709c43921600e9dc28
nikhil264 <nikhilreddy323@gmail.com>
2017-03-17 00:44:33 +0530

    added fetching data from next page

--- scraper.go
+++ scraper.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -23,6 +24,11 @@ type SubReddit struct {
 }
 
 var err error
+
+const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8"
+
+//wg wait group for all downloads to complete
+var wg sync.WaitGroup
 
 //ScrapeImages downloads #num images from the given subreddit tab
 //into a new folder at path
@@ -41,17 +47,52 @@ func (r SubReddit) ScrapeImages(tab string, num uint8, path string, linksFrom st
 
 }
 
+var baseURL string
+
 //LinkCrawl finds the required links
-func LinkCrawl(baseURL string) {
-	doc, err := goquery.NewDocument(baseURL)
+func LinkCrawl(url string) {
+	baseURL = url
+	linkCrawler(url)
+}
+
+func linkCrawler(url string) {
+	req, err := http.NewRequest("GET", url, nil)
+	HandleErr(err)
+	req.Header.Set("User-Agent", userAgent)
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromResponse(resp)
 	HandleErr(err)
 
-	doc.Find(".siteTable").Each(func(index int, item *goquery.Selection) {
-		linkTag := item.Find("div")
-		after, _ := linkTag.Attr("data-fullname")
-		link, _ := linkTag.Attr("data-url")
-		fmt.Printf("%s  %s\n", after, link)
+	//finds the data urls from the entire page
+	links := doc.Find("#siteTable div").Map(func(index int, item *goquery.Selection) string {
+		link, _ := item.Attr("data-url")
+		return link
 	})
+
+	//finds url of the next button
+	var next string
+	doc.Find("span .next-button a").Each(func(index int, item *goquery.Selection) {
+		next, _ = item.Attr("href")
+		fmt.Println(next)
+	})
+
+	//downloads each data url link if its a file
+	for _, v := range links {
+		if len(v) > 5 && v[len(v)-4:len(v)-3] == "." {
+			wg.Add(1)
+			go Download(v, "/Users/Reddy/gg")
+		}
+	}
+
+	wg.Add(1)
+	go linkCrawler(next)
+	wg.Wait()
 }
 
 //HandleErr handles error
@@ -63,16 +104,21 @@ func HandleErr(err error) {
 
 //Download s from the url  and saves in path folder
 func Download(url string, path string) {
-	r, err := http.Get(url)
+	defer wg.Done()
+	// req, err := http.NewRequest("GET", url, nil)
+	// HandleErr(err)
+	// req.Header.Set("User-Agent", userAgent)
+	// c := &http.Client{}
+	resp, err := http.Get(url)
 	HandleErr(err)
-	defer r.Body.Close()
+	defer resp.Body.Close()
 	tmp := strings.SplitAfter(url, "/")
 	path = filepath.Join(path, tmp[len(tmp)-1])
 	file, err := os.Create(path)
 	defer file.Close()
 	HandleErr(err)
 
-	_, err = io.Copy(file, r.Body)
+	_, err = io.Copy(file, resp.Body)
 	HandleErr(err)
 
 }
