69e70470bc574fccca3fbc6fbc53f025b38b9037
nikhil264 <nikhilreddy323@gmail.com>
2018-02-19 19:58:49 +0530

    removed err checking for simplicity

--- rscraper.go
+++ rscraper.go
@@ -8,13 +8,18 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8"
 
-var wg sync.WaitGroup
+var downloadsWg sync.WaitGroup
+
+var n int // no of remaining downloads.
+var total int
+var baseURL string
 
 func downloadbleLinks(links []string) (dlinks []string) {
 	//downloads each data url link if its a file
@@ -26,16 +31,16 @@ func downloadbleLinks(links []string) (dlinks []string) {
 				if strings.Contains(v, "imgur.com/gallery/") {
 					t := strings.Replace(v, "gallery", "a", -1) + "/zip"
 					dlinks = append(dlinks, t)
-					v = strings.Replace(v, "gallery", "download", -1)
-					dlinks = append(dlinks, v)
+					t = strings.Replace(v, "gallery", "download", -1)
+					dlinks = append(dlinks, t)
 				}
 				if strings.Contains(v, "imgur.com/a/") {
-					v = v + "/zip"
-					dlinks = append(dlinks, v)
+					t := v + "/zip"
+					dlinks = append(dlinks, t)
 				}
 				if strings.Contains(v, "imgur.com/download/") {
-					v = strings.Replace(v, "/download", "", -1)
-					dlinks = append(dlinks, v)
+					t := strings.Replace(v, "/download", "", -1)
+					dlinks = append(dlinks, t)
 				}
 			}
 		}
@@ -43,42 +48,34 @@ func downloadbleLinks(links []string) (dlinks []string) {
 	return dlinks
 }
 
-//LinkCrawl finds the required links
-func LinkCrawl(url string, path string) (err error) {
-	wg.Add(1)
-	err = os.MkdirAll(path, os.ModePerm)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	err = os.Chdir(path)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
+//LinkCrawl finds the required links and creates a folder to store the downloaded files
+func LinkCrawl(url string, path string) {
+	baseURL = url
+	f, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	//intially no downloads are remaining
+	n = 0
+	total = 0
+	os.MkdirAll(path, os.ModePerm)
+	os.Chdir(path)
+
 	linkCrawler(url)
-	return err
 }
 
-func linkCrawler(url string) (err error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
+func linkCrawler(url string) {
+	req, _ := http.NewRequest("GET", url, nil)
+
 	req.Header.Set("User-Agent", userAgent)
 	c := &http.Client{}
 	resp, err := c.Do(req)
 	if err != nil {
 		log.Fatal(err)
-		return err
 	}
 	defer resp.Body.Close()
-	doc, err := goquery.NewDocumentFromResponse(resp)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
+	doc, _ := goquery.NewDocumentFromResponse(resp)
 
 	//finds the data urls from the entire page
 	links := doc.Find("#siteTable div").Map(func(index int, item *goquery.Selection) string {
@@ -87,47 +84,50 @@ func linkCrawler(url string) (err error) {
 	})
 
 	links = downloadbleLinks(links)
-	println(len(links))
+
 	for _, v := range links {
-		wg.Add(1)
-		go Download(v)
+		downloadsWg.Add(1)
+		n++
+		// go Download(v)
+		go fake(v)
 	}
 
 	//finds url of the next button
 	var next string
 	doc.Find("span .next-button a").Each(func(index int, item *goquery.Selection) {
 		next, _ = item.Attr("href")
-		fmt.Println(next)
+		fmt.Println(strings.TrimPrefix(next, baseURL))
+		log.Printf(next)
 	})
 
-	if len(next) > 0 {
-		go linkCrawler(next)
-	} else {
-		wg.Done()
-		println("crawling done")
+	for n >= 0 && len(next) > 0 {
+		if n < 101 {
+			linkCrawler(next)
+		} else {
+			time.Sleep(time.Second * 5)
+			fmt.Println("Downloaded ", total, " items.")
+		}
 	}
-	wg.Wait()
-
-	return err
+	downloadsWg.Wait()
+	println(n)
+	println("crawling done Downloaded ", total, " items.")
 }
 
 //Download saves the contents from url into file
 func Download(url string) (err error) {
-	defer wg.Done()
+	defer downloadsWg.Done()
+
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 	contentType := resp.Header.Get("Content-Type")
+
 	filename := ""
-	if contentType == "application/zip" {
+	if strings.Contains(contentType, "zip") || strings.Contains(contentType, "image") {
 		filename = resp.Header.Get("Content-Disposition")
 		//error may occur if split doesnt zero length slice
-		filename = strings.Split(filename, "\"")[1]
-	}
-	if strings.Contains(contentType, "image") {
-		filename = resp.Header.Get("Content-Disposition")
 		if filename == "" {
 			tmp := strings.Split(url, "/")
 			filename = tmp[len(tmp)-1]
@@ -136,20 +136,22 @@ func Download(url string) (err error) {
 		}
 
 	}
+
 	if filename != "" {
-		file, err := os.Create(filename)
-		if err != nil {
-			log.Println(err)
-			return err
-		}
+		file, _ := os.Create(filename + ".part")
 		defer file.Close()
 
-		_, err = io.Copy(file, resp.Body)
-		if err != nil {
-			log.Println(err)
-			return err
-		}
+		io.Copy(file, resp.Body)
+		os.Rename(filename+".part", filename)
+		log.Printf(filename + "  " + url)
 	}
-
+	n--
+	total++
 	return err
+}
+
+func fake(url string) {
+	defer downloadsWg.Done()
+	n--
+	total++
 }
